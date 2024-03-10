50206a16ba9defc93db1b415730a3a1566485580
Kishore2071 <kishorekumarsaran2001@gmail.com>
2024-03-10 15:19:57 +0530

    Completed Basic Setup

--- Hotstart_2.0/App.js
+++ Hotstart_2.0/App.js
@@ -1,20 +1,47 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from "styled-components";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render(){
+    return (
+      <Main>
+        <StatusBar hidden/>
+        <Header>
+          <Logo>Hotstar</Logo>
+          <Profile />
+        </Header>
+      </Main>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Main = styled.View`
+  flex: 1;
+  background-color: #E5E5E5;
+
+`;
+
+const Header = styled.View`
+  height: 56px;
+  width: 100%;
+  background-color: #FFF;
+`;
+
+const Profile = styled.Image`
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  width: 40px;
+  height: 40px;
+  background: #D9D9D9;
+  border-radius: 20px;
+`;
+
+const Logo = styled.Text`
+  margin-top: 10px;
+  margin-left: 10px;
+  color: black;
+  font-size: 25px;
+`;
